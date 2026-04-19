91bb3d804c70c5fa2a35976a573264224bf12cd4
Vasa346 <vasilevsvasa3478@gmail.com>
2026-04-19 19:11:25 +0500

    Решено задание 00_distance

--- 00_distance.py
+++ 00_distance.py
@@ -4,9 +4,9 @@
 # Есть словарь координат городов
 
 sites = {
-    'Moscow': (550, 370),
-    'London': (510, 510),
-    'Paris': (480, 480),
+    "Moscow": (550, 370),
+    "London": (510, 510),
+    "Paris": (480, 480),
 }
 
 # Составим словарь словарей расстояний между ними
@@ -15,9 +15,12 @@ sites = {
 distances = {}
 
 # TODO здесь заполнение словаря
+for city1 in sites:
+    distances[city1] = {}
+    for city2 in sites:
+        x1, y1 = sites[city1]
+        x2, y2 = sites[city2]
+        distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+        distances[city1][city2] = round(distance, 2)
 
 print(distances)
-
-
-
-
